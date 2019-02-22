2cdb13eb134dceb118ec77167ff60ef0123f473b
Lyu Shuyuan <779455514@qq.com>
2019-02-22 10:28:40 +0100

    3.2 - Rendu minimal not finished

--- TP2/Scene.h
+++ TP2/Scene.h
@@ -12,9 +12,10 @@
 #include "Light.h"
 
 /// Namespace RayTracer
-namespace rt {
+namespace rt
+{
 
-  /**
+/**
   Models a scene, i.e. a collection of lights and graphical objects
   (could be a tree, but we keep a list for now for the sake of
   simplicity).
@@ -23,75 +24,97 @@ namespace rt {
   is thus responsible for its deallocation.
   */
 
-  struct Scene {
+struct Scene
+{
     /// The list of lights modelled as a vector.
-    std::vector< Light* > myLights;
+    std::vector<Light *> myLights;
     /// The list of objects modelled as a vector.
-    std::vector< GraphicalObject* > myObjects;
+    std::vector<GraphicalObject *> myObjects;
 
     /// Default constructor. Nothing to do.
     Scene() {}
 
     /// Destructor. Frees objects.
-    ~Scene() 
+    ~Scene()
     {
-      for ( Light* light : myLights )
-        delete light;
-      for ( GraphicalObject* obj : myObjects )
-        delete obj;
-      // The vector is automatically deleted.
+        for (Light *light : myLights)
+            delete light;
+        for (GraphicalObject *obj : myObjects)
+            delete obj;
+        // The vector is automatically deleted.
     }
 
     /// This function calls the init method of each of its objects.
-    void init( Viewer& viewer )
+    void init(Viewer &viewer)
     {
-      for ( GraphicalObject* obj : myObjects )
-        obj->init( viewer );
-      for ( Light* light : myLights )
-        light->init( viewer );
+        for (GraphicalObject *obj : myObjects)
+            obj->init(viewer);
+        for (Light *light : myLights)
+            light->init(viewer);
     }
     /// This function calls the draw method of each of its objects.
-    void draw( Viewer& viewer )
+    void draw(Viewer &viewer)
     {
-      for ( GraphicalObject* obj : myObjects )
-        obj->draw( viewer );
-      for ( Light* light : myLights )
-        light->draw( viewer );
+        for (GraphicalObject *obj : myObjects)
+            obj->draw(viewer);
+        for (Light *light : myLights)
+            light->draw(viewer);
     }
     /// This function calls the light method of each of its lights
-    void light(Viewer& viewer )
+    void light(Viewer &viewer)
     {
-      for ( Light* light : myLights )
-        light->light( viewer );
+        for (Light *light : myLights)
+            light->light(viewer);
     }
 
     /// Adds a new object to the scene.
-    void addObject( GraphicalObject* anObject )
+    void addObject(GraphicalObject *anObject)
     {
-      myObjects.push_back( anObject );
+        myObjects.push_back(anObject);
     }
 
     /// Adds a new light to the scene.
-    void addLight( Light* aLight )
+    void addLight(Light *aLight)
     {
-      myLights.push_back( aLight );
+        myLights.push_back(aLight);
     }
-    
+
     /// returns the closest object intersected by the given ray.
     Real
-    rayIntersection( const Ray& ray,
-                     GraphicalObject*& object, Point3& p )
+    rayIntersection(const Ray &ray,
+                    GraphicalObject *&object, Point3 &p)
     {
-      object = 0;
-      return 1.0f;
+        Point3 pDeBase;
+        myObjects[0] -> rayIntersection(ray, pDeBase);
+        float distance = distance2(pDeBase, ray.origin);
+        for (std::vector<GraphicalObject *>::const_iterator it = myObjects.begin(); it < myObjects.end(); ++it)
+        {
+            Point3 newP;
+            (*it)->rayIntersection(ray, newP);
+            // qp . w
+            if (distance2(newP, ray.origin) < distance)
+            {
+                p = newP;
+                object = *it;
+            }
+        }
+        if (object == NULL)
+        {
+            object = 0;
+            return 1.0f;
+        }
+        else
+        {
+            return -1.0f;
+        }
     }
 
   private:
     /// Copy constructor is forbidden.
-    Scene( const Scene& ) = delete;
+    Scene(const Scene &) = delete;
     /// Assigment is forbidden.
-    Scene& operator=( const Scene& ) = delete;
-  };
+    Scene &operator=(const Scene &) = delete;
+};
 
 } // namespace rt
 
--- TP2/Sphere.cpp
+++ TP2/Sphere.cpp
@@ -4,93 +4,119 @@
 #include <cmath>
 #include "Sphere.h"
 
-void
-rt::Sphere::draw( Viewer& /* viewer */ )
+void rt::Sphere::draw(Viewer & /* viewer */)
 {
   Material m = material;
   // Taking care of south pole
-  glBegin( GL_TRIANGLE_FAN );
-  glColor4fv( m.ambient );
+  glBegin(GL_TRIANGLE_FAN);
+  glColor4fv(m.ambient);
   glMaterialfv(GL_FRONT, GL_DIFFUSE, m.diffuse);
   glMaterialfv(GL_FRONT, GL_SPECULAR, m.specular);
-  glMaterialf(GL_FRONT, GL_SHININESS, m.shinyness );
-  Point3 south_pole = localize( -90, 0 );
-  glNormal3fv( getNormal( south_pole ) );
-  glVertex3fv( south_pole );
-  for ( int x = 0; x <= NLON; ++x )
-    {
-      Point3 p = localize( -90 + 180/NLAT, x * 360 / NLON );
-      glNormal3fv( getNormal( p ) );
-      glVertex3fv( p );
-    }
+  glMaterialf(GL_FRONT, GL_SHININESS, m.shinyness);
+  Point3 south_pole = localize(-90, 0);
+  glNormal3fv(getNormal(south_pole));
+  glVertex3fv(south_pole);
+  for (int x = 0; x <= NLON; ++x)
+  {
+    Point3 p = localize(-90 + 180 / NLAT, x * 360 / NLON);
+    glNormal3fv(getNormal(p));
+    glVertex3fv(p);
+  }
   glEnd();
   // Taking care of in-between poles
-  for ( int y = 1; y < NLAT - 1; ++y )
+  for (int y = 1; y < NLAT - 1; ++y)
+  {
+    glBegin(GL_QUAD_STRIP);
+    glColor4fv(m.ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, m.diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, m.specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, m.shinyness);
+    for (int x = 0; x <= NLON; ++x)
     {
-      glBegin( GL_QUAD_STRIP);
-      glColor4fv( m.ambient );
-      glMaterialfv(GL_FRONT, GL_DIFFUSE, m.diffuse);
-      glMaterialfv(GL_FRONT, GL_SPECULAR, m.specular);
-      glMaterialf(GL_FRONT, GL_SHININESS, m.shinyness );
-      for ( int x = 0; x <= NLON; ++x )
-        {
-          Point3 p = localize( -90 + y*180/NLAT,     x * 360 / NLON );
-          Point3 q = localize( -90 + (y+1)*180/NLAT, x * 360 / NLON );
-          glNormal3fv( getNormal( p ) );
-          glVertex3fv( p );
-          glNormal3fv( getNormal( q ) );
-          glVertex3fv( q );
-        }
-      glEnd();
+      Point3 p = localize(-90 + y * 180 / NLAT, x * 360 / NLON);
+      Point3 q = localize(-90 + (y + 1) * 180 / NLAT, x * 360 / NLON);
+      glNormal3fv(getNormal(p));
+      glVertex3fv(p);
+      glNormal3fv(getNormal(q));
+      glVertex3fv(q);
     }
+    glEnd();
+  }
   // Taking care of north pole
-  glBegin( GL_TRIANGLE_FAN );
-  glColor4fv( m.ambient );
+  glBegin(GL_TRIANGLE_FAN);
+  glColor4fv(m.ambient);
   glMaterialfv(GL_FRONT, GL_DIFFUSE, m.diffuse);
   glMaterialfv(GL_FRONT, GL_SPECULAR, m.specular);
-  glMaterialf(GL_FRONT, GL_SHININESS, m.shinyness );
-  Point3 north_pole = localize( 90, 0 );
-  glNormal3fv( getNormal( north_pole ) );
-  glVertex3fv( north_pole );
-  for ( int x = NLON; x >= 0; --x )
-    {
-      Point3 p = localize( -90 + (NLAT-1)*180/NLAT, x * 360 / NLON );
-      glNormal3fv( getNormal( p ) );
-      glVertex3fv( p );
-    }
+  glMaterialf(GL_FRONT, GL_SHININESS, m.shinyness);
+  Point3 north_pole = localize(90, 0);
+  glNormal3fv(getNormal(north_pole));
+  glVertex3fv(north_pole);
+  for (int x = NLON; x >= 0; --x)
+  {
+    Point3 p = localize(-90 + (NLAT - 1) * 180 / NLAT, x * 360 / NLON);
+    glNormal3fv(getNormal(p));
+    glVertex3fv(p);
+  }
   glEnd();
 }
 
 rt::Point3
-rt::Sphere::localize( Real latitude, Real longitude ) const
+rt::Sphere::localize(Real latitude, Real longitude) const
 {
   static const Real conv_deg_rad = 2.0 * M_PI / 360.0;
-  latitude  *= conv_deg_rad;
+  latitude *= conv_deg_rad;
   longitude *= conv_deg_rad;
-  return center 
-    + radius * Point3( cos( longitude ) * cos( latitude ),
-                       sin( longitude ) * cos( latitude ),
-                       sin( latitude ) );
+  return center + radius * Point3(cos(longitude) * cos(latitude),
+                                  sin(longitude) * cos(latitude),
+                                  sin(latitude));
 }
 
 rt::Vector3
-rt::Sphere::getNormal( Point3 p )
+rt::Sphere::getNormal(Point3 p)
 {
   Vector3 u = p - center;
-  Real   l2 = u.dot( u );
-  if ( l2 != 0.0 ) u /= sqrt( l2 );
+  Real l2 = u.dot(u);
+  if (l2 != 0.0)
+    u /= sqrt(l2);
   return u;
 }
 
 rt::Material
-rt::Sphere::getMaterial( Point3 /* p */ )
+    rt::Sphere::getMaterial(Point3 /* p */)
 {
   return material; // the material is constant along the sphere.
 }
 
 rt::Real
-rt::Sphere::rayIntersection( const Ray& ray, Point3& p )
+rt::Sphere::rayIntersection(const Ray &ray, Point3 &p)
 {
-  // TO DO
-  return 1.0f;
+  // calculez la distance au carré entre c et la droite contenant le rayon [o,u)
+  Vector3 origineCenter = center - ray.origin;
+  Vector3 origineOrtho = origineCenter.dot(ray.direction) * ray.direction;
+  float distance = distance2(origineCenter, origineOrtho);
+  // si cette distance au carré est plus grande que r^2, alors il n'y pas d'intersection et on retourne un nombre positif.
+  if (distance > radius * radius)
+    return 1.0f;
+  else
+  {
+    // delta = b^2 - 4ac
+    Vector3 centerOrigin = ray.origin - center;
+    float discriminant = 4 * (ray.direction.dot(centerOrigin)) * (ray.direction.dot(centerOrigin)) - 4 * (centerOrigin.dot(centerOrigin) - radius * radius);
+    float t1 = (-2 * ray.direction.dot(centerOrigin) - sqrt(discriminant)) / 2;
+    float t2 = (-2 * ray.direction.dot(centerOrigin) + sqrt(discriminant)) / 2;
+    // Point3 point1 = ray.origine + t1 * ray.direction;
+    // Point3 point2 = ray.origine + t2 * ray.direction;
+    if (t1 > 0)
+    {
+      p = ray.origin + t1 * ray.direction;
+      return -1.0f;
+    }
+    else if (t2 > 0)
+    {
+      p = ray.origin + t2 * ray.direction;
+      return -1.0f;
+    }
+    else
+      return 1.0f;
+  }
 }
